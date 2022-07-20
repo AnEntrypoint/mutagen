7e03bd85937fbbff223733c0f6535c92d5b36b10
lanmower <almagestfraternite@gmail.com>
2022-07-20 21:24:19 +0000

    more mutability

--- client/uploader.js
+++ client/uploader.js
@@ -1,22 +1,18 @@
 require('dotenv').config();
 const { getFilesFromPath, Web3Storage, File  } = require('web3.storage');
 const hre = require("hardhat");
-const axios = require('axios');
 
 (async ()=>{
-  
     const storage = new Web3Storage({ token:process.env.WEB3STORAGETOKEN })
     const file = await getFilesFromPath('public');
-    console.log(file);
     const files = file.map(a=>{a.name = a.name.replace('/public',''); return a;});
-    console.log(files);
     const cid = await storage.put(files);
     const newcid = await storage.put([
         new File(
           [JSON.stringify({
-            name: "moonshine",
-            description: "Paper collage, using salvaged and original watercolour papers", 
-            image: "https://ipfs.infura.io/ipfs/QmWc6YHE815F8kExchG9kd2uSsv7ZF1iQNn23bt5iKC6K3/image",
+            name: process.env.NAME,
+            description: process.env.DESCRIPTION, 
+            image: process.env.IMAGE,
             site: 'https://'+cid+'.ipfs.dweb.link/'
           })],
           '/metadata.json'
@@ -27,6 +23,5 @@ const axios = require('axios');
     const data = await token.getData(process.env.NAME);
     if(data) console.log(await token.setAddress(process.env.NAME, 'https://'+newcid+'.ipfs.dweb.link/metadata.json'));
     else console.log(await token.mintToken(process.env.NAME, 'https://'+newcid+'.ipfs.dweb.link/metadata.json'));
-    
 })();
 
